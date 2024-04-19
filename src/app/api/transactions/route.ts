d3b00d1ac128f62756e604f28cab551f7c3d5b2d
Mateus Henrique <mateusdzn10@gmail.com>
2024-04-19 00:17:54 -0300

    feat: :sparkles: add new route to create transaction

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,6 +1,9 @@
 import prisma from '~/lib/database';
 import { NextResponse } from 'next/server';
 
+import type { TTransactionDTO } from '~/services/transactions';
+import { generateNumberUid } from '~/utils/uid';
+
 export async function GET() {
   try {
     const transactions = await prisma.transaction.findMany();
@@ -8,5 +11,20 @@ export async function GET() {
     return NextResponse.json({ transactions });
   } catch (error) {
     return NextResponse.json({ error });
+  }
+}
+
+export async function POST(transactionDTO: TTransactionDTO) {
+  try {
+    await prisma.transaction.create({
+      data: {
+        ...transactionDTO,
+        id: generateNumberUid(),
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.json({ error, success: false });
   }
 }
