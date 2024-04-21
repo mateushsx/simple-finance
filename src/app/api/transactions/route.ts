1931990312ec71623115a28f14edc1c107f47775
Mateus Henrique <mateusdzn10@gmail.com>
2024-04-20 23:20:02 -0300

    fix: :bug: transaction creation search routes

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,24 +1,38 @@
 import prisma from '~/lib/database';
-import { NextResponse } from 'next/server';
 
-import type { TTransactionDTO } from '~/services/transactions';
+import { NextRequest, NextResponse } from 'next/server';
 import { generateNumberUid } from '~/utils/uid';
 
-export async function GET() {
+import type { TTransactionDTO } from '~/services/transactions';
+
+export async function GET(req: NextRequest, res: NextResponse) {
   try {
     const transactions = await prisma.transaction.findMany();
+    const transactionsData = transactions.map((transaction) => {
+      return {
+        ...transaction,
+        id: Number(transaction.id),
+        date: transaction.date.toISOString(),
+        amount: Number(transaction.amount),
+      };
+    });
 
-    return NextResponse.json({ transactions });
+    return NextResponse.json({ transactions: transactionsData });
   } catch (error) {
     return NextResponse.json({ error });
   }
 }
 
-export async function POST(transactionDTO: TTransactionDTO) {
+export async function POST(req: NextRequest, res: NextResponse) {
+  const body = await req.json();
+  const transactionDTO = body as TTransactionDTO;
+
   try {
     await prisma.transaction.create({
       data: {
         ...transactionDTO,
+        date: new Date(transactionDTO.date),
+        amount: Number(transactionDTO.amount),
         id: generateNumberUid(),
       },
     });
